420b92d39202de65e81b378e24bcbf1de5e7328b
dayano <kiixxn00@gmail.com>
2025-08-11 11:48:56 +0900

    remove unused code for input handling and calculations in main function

--- templete/d.cpp
+++ templete/d.cpp
@@ -4,17 +4,6 @@ using namespace std;
 using ll = long long;
 
 int main() {
-    int n;
-    string s;
-    cin >> n >> s;
-
-    vector<int> p;
-    rep(i, n) if (s[i] == '1') p.push_back(i);
-    int k = p.size();
-    rep(i, k) p[i] -= i;
-    int med = p[k / 2];
-    ll ans = 0;
-    rep(i, k) ans += abs(med - p[i]);
-    cout << ans << endl;
+    //
     return 0;
 }
